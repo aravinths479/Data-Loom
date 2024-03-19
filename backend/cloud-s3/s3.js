7664fd6eb2b2a05dc47bd6e5c9fc22d4e8d8a6be
ARAVINTH <78143185+aravinths479@users.noreply.github.com>
2024-03-19 14:51:06 +0530

    Update s3.js

--- backend/cloud-s3/s3.js
+++ backend/cloud-s3/s3.js
@@ -9,15 +9,6 @@ const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 
 
 
-// Configure AWS
-const s3 = new S3Client({
-    region: 'ap-south-1',
-  credentials: {
-    accessKeyId: 'AKIASGQARUQKOR4HBPUI',
-  secretAccessKey: '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',
- 
-  },
-});
 
 // Configure Multer for file uploads
 const upload = multer({
